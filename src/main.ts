7811e663d5ba95385a31035e583efb96616e1f77
Dmitry Melnikov <m1290d@gmail.com>
2020-10-07 01:11:34 +0500

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { getProjectMeta } from './project-meta';
 
 const PORT = 3000;
 
 async function bootstrap() {
+  const { description, version }  = await getProjectMeta();
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Expense tracker for YNAB')
+    .setDescription(description)
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
   Logger.log(`App started on port ${PORT}`);
 }
+
+// process.on('unhandledRejection')
+
 bootstrap();
---
+++ src/project-meta.ts
@@ -0,0 +1,17 @@
+import readPackageUp = require('read-pkg-up');
+
+export async function getProjectMeta() {
+    const result = await readPackageUp();
+
+    if(!result) {
+        throw new Error('Unable to get project metadata');
+    }
+
+    const { packageJson } = result;
+    const { version, description } = packageJson;
+
+    return {
+        version,
+        description,
+    }    
+}
